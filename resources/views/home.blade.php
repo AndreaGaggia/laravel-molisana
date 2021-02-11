8d890e6aaa8589852a7fe705b5d39c67532c924e
AndreaG <gaggiandrea@gmail.com>
2021-02-11 15:20:20 +0100

    basic template

---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.base')
+@section('content')
+<h1>Eccoci</h1>
+@endsection
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,8 @@
+<header>
+    <img src="{{ asset('img/logo-molisana.png') }}" />
+    <nav class="links">
+        <a href="/">home</a>
+        <a href="/prodotti">prodotti</a>
+        <a href="/contatti">contatti</a>
+    </nav>
+</header>
