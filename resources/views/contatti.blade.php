1aab4650de6eb88c370cffb8a025d9d9b4ca4433
AndreaGaggia <gaggiandrea@gmail.com>
2021-02-12 16:30:15 +0100

    contatti page and better single product view

--- resources/views/contatti.blade.php
+++ resources/views/contatti.blade.php
@@ -5,5 +5,9 @@ CONTATTI
 @endsection
 
 @section('content')
-<h1>CONTATTI</h1>
+<img src="{{ asset('img/map.png') }}" alt="mappa sede" width=100%>
+<div class="address">
+    <h2>C.da Colle delle Api 100/A <br> 86100 CAMPOBASSO</h2>
+    <h2>numero verde: 800.81.80.81 <br> whatsapp: 380.12.92.455</h2>
+</div>
 @endsection
--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -5,7 +5,13 @@
 @endsection
 
 @section('content')
-<h1>{{ $prodotto['titolo'] }}</h1>
-<h2>Cottura: {{ $prodotto['cottura'] }}</h2>
-<div>{!! $prodotto['descrizione'] !!}</div>
+<div style="background-color: lightyellow; padding: 1rem; border-radius: .5rem; box-shadow: 0 0 16px 1px lightgrey">
+    <a href="{{ route('prodotti-pro') }}"
+        style="display: block; text-decoration: none; font-weight: bold; margin-bottom: 2rem">⬅️ Back
+        to pro</a>
+    <img src="{{ $prodotto['src'] }}" width=400>
+    <h1 style="margin-top: 1rem">{{ $prodotto['titolo'] }}</h1>
+    <h2>Cottura: {{ $prodotto['cottura'] }}</h2>
+    <p style="margin-top: 2rem; font-size: 1.1rem">{!! $prodotto['descrizione'] !!}</p>
+</div>
 @endsection
